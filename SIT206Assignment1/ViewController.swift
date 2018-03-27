91db0325d0f3ca94232a6a3184a5846e419c8241
Kelvin Salim <kelvin.s.ks19@gmail.com>
2018-03-27 13:32:05 +1100

    Initial Commit

---
+++ SIT206Assignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIT206Assignment1
+//
+//  Created by Kelvin Salim on 27/3/18.
+//  Copyright © 2018 Kelvin Salim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
